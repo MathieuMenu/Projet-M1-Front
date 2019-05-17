6f5a64b600ab7bc16de3965f22df5f75bfc1e192
MathieuMenu <mathieu.menu02@gmail.com>
2019-05-17 20:59:41 +0200

    Test service MLAB avec le back

--- server.js
+++ server.js
@@ -5,11 +5,26 @@ const express = require('express');
 
 const path = require('path');
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
 // Serve only the static files form the dist directory
 
 app.use(express.static(__dirname + '/dist/mmfront'));
+
+app.use(bodyParser());
+app.use(bodyParser.json({limit:'5mb'}));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use(function (req, res, next){
+	res.setHeader('Access-Control-Allow-Origin', '');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+})
 
 app.get('/*', function(req,res) {
 
