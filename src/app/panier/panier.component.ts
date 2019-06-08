008825d60961c3bb27dd211454e9adecd6ec4876
MathieuMenu <mathieu.menu02@gmail.com>
2019-06-08 15:40:13 +0200

    Premier test d'agenda

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -37,9 +37,28 @@ export class PanierComponent implements OnInit {
   trie = function(){
     var Locations2 = this.Locations;
 
-    Locations2.sort(function(a, b){return 0.5 - Math.random()});
+    this.shuffle(Locations2);
 
     console.log(Locations2);
   }
+
+  shuffle = function (array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
 
 }
