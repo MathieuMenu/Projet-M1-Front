9476081d7ffc29635d419eb3d50ac80a05a2db7c
MathieuMenu <mathieu.menu02@gmail.com>
2019-06-20 20:53:27 +0200

    Amélioration du table

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -16,6 +16,7 @@ export class PanierComponent implements OnInit {
   Locations2: any;
   Distance: any = [];
   directionsService: any;
+  letest:any;
   constructor(private mongoservice: MongoService, public auth: AuthService,private mapsAPILoader: MapsAPILoader) { }
 
   ngOnInit() {
@@ -40,7 +41,8 @@ export class PanierComponent implements OnInit {
     
     this.directionsService.route({origin:originlat+","+originlong, destination:destinationlat+","+destinationlong, travelMode:google.maps.TravelMode.DRIVING}, function(result, status){
       if(status == google.maps.DirectionsStatus.OK){
-        this.this.log(result.routes[0].legs[0].distance.text);
+        this.this.letest = (result.routes[0].legs[0].distance.text);
+        console.log(this.this.letest);
       }
     });
     
