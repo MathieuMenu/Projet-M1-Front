6a83071395a8abecadda17a3108eb9659d02a562
MathieuMenu <mathieu.menu02@gmail.com>
2019-06-20 15:57:06 +0200

    Amélioration du table

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 app.use(function (req, res, next){
-	res.setHeader('Access-Control-Allow-Origin', 'https://mmback.herokuapp.com');
-	res.setHeader('Access-Control-Allow-Origin', 'https://mmfront.herokuapp.com');
+	var origin = req.headers.origin;
+	res.setHeader('Access-Control-Allow-Origin', origin);
+	//res.setHeader('Access-Control-Allow-Origin', 'https://mmback.herokuapp.com');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
 	res.setHeader('Access-Control-Allow-Credentials', true);
